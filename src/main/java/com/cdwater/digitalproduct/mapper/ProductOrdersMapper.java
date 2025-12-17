7937c0f57aaa2a526cb372420218e7cd23b04f41
ZhouXun666 <3250267927@qq.com>
2025-12-17 23:28:41 +0800

    完成与产品管理相关数据库操作方法的声明

---
+++ src/main/java/com/cdwater/digitalproduct/mapper/ProductOrdersMapper.java
@@ -0,0 +1,49 @@
+package com.cdwater.digitalproduct.mapper;
+
+import com.cdwater.digitalproduct.entity.ProductOrders;
+import com.cdwater.digitalproduct.model.dto.ProductOrdersPageDTO;
+import com.cdwater.digitalproduct.model.vo.ProductOrdersPageVO;
+import com.cdwater.digitalproduct.model.vo.ProductOrdersShowVO;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Map;
+
+public interface ProductOrdersMapper {
+
+    List<ProductOrdersPageVO> selectPage(ProductOrdersPageDTO productOrdersPageDTO);
+
+    void insert(ProductOrders productOrders);
+
+    ProductOrders selectByOrderNo(Long orderNo);
+
+    void updateById(ProductOrders productOrders);
+
+    List<ProductOrdersShowVO> selectByUserId(Integer userId);
+
+    List<ProductOrders> selectByOrderStatusAndBeforeTime(Integer orderStatus, LocalDateTime time);
+
+    /**
+     * 统计相关
+     */
+    BigDecimal sumByOrderStatus();
+
+    @SuppressWarnings("MybatisXMapperMethodInspection")
+    List<Map<String, Object>> getShopAmountMap();
+
+    @SuppressWarnings("MybatisXMapperMethodInspection")
+    List<Map<String, Object>> selectTodayDataByShopId(Integer shopId);
+
+    List<BigDecimal> selectRangeAmountDataByShopId(Integer shopId, List<LocalDate> dateList);
+
+    @SuppressWarnings("MybatisXMapperMethodInspection")
+    List<Map<String, Long>> selectRangeNumberDataByShopId(Integer shopId, List<LocalDate> dateList);
+
+    Integer countByShopIdAndOrderStatus(Integer shopId, Integer orderStatus);
+}
+
+
+
+
